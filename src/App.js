cca68ed3be7f04daef19b97fcb17232e581162b1
divyav26 <divyam2633@gmail.com>
2024-07-14 19:05:37 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './components/Home';
+import Success from './components/Success';
+import Error from './components/Error';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter >
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/success' element={<ProtectedRoute element={<Success/>}/>}/>
+        <Route path='/*' element={<Error/>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
